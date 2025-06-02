dfa1628dde22dc012b0f2687dd56e7b74d2310c1
Anh Nguyen <panhhng@gmail.com>
2025-06-02 15:19:51 -0400

    updated README

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-userkey = "nguyho03@gettysburg.edu"
-password = "P@nh2025app"
-applicationkey = "451ddbe5-25fe-4281-9dc7-ad3200e6f631"
-servername = "gettysburg.hosted.panopto.com"
+userkey = ""
+password = ""
+applicationkey = ""
+servername = ""
